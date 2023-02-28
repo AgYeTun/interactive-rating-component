def329affc79da7f53d8badfbe26c92727bc3a02
AungYeTun <aungyetuninsane@gmail.com>
2023-02-28 22:28:47 +0630

    svg third fix

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import { ReactSVG } from 'react-svg'
+import { useNavigate } from 'react-router-dom'
 
 const Feedback = () => {
     const [rate, setRate] = useState('')
@@ -17,7 +16,7 @@ const Feedback = () => {
   return (
     <div className={`w-[400px] h-fit bg-dark-blue p-[36px] mx-5 flex flex-col gap-3 rounded-[30px] shadow-lg ${active && 'animate__animated animate__zoomOut'}`}>
         <div className="w-12 h-12 bg-medium-grey flex justify-center items-center rounded-full mb-4">
-            <ReactSVG src="../src/assets/images/icon-star.svg" />
+            <svg width="17" height="16" xmlns="http://www.w3.org/2000/svg"><path d="m9.067.43 1.99 4.031c.112.228.33.386.58.422l4.45.647a.772.772 0 0 1 .427 1.316l-3.22 3.138a.773.773 0 0 0-.222.683l.76 4.431a.772.772 0 0 1-1.12.813l-3.98-2.092a.773.773 0 0 0-.718 0l-3.98 2.092a.772.772 0 0 1-1.119-.813l.76-4.431a.77.77 0 0 0-.222-.683L.233 6.846A.772.772 0 0 1 .661 5.53l4.449-.647a.772.772 0 0 0 .58-.422L7.68.43a.774.774 0 0 1 1.387 0Z" fill="#FC7614"/></svg>
         </div>
         <h1 className='text-white text-2xl'>How did we do?</h1>
         <p className='text-light-grey text-[15px]'>Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
